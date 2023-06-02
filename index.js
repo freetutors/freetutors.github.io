4df2503f3eb721f1455c81ccb6e93af581ab7544
Aarush Parikh <parikhaarush@gmail.com>
2023-06-02 11:08:50 -0700

    Added sign up options. Please change colors and add stuff for the login.html. Sign up with google will have to wait

---
+++ index.js
@@ -0,0 +1,10 @@
+//setting login button to redirect
+document.querySelector(".login-button").
+addEventListener("click", () => {
+    window.location='login.html'
+}); 
+//setting signup button to redirect
+document.querySelector(".signup-button").
+addEventListener("click", () => {
+    window.location='signup.html'
+});
