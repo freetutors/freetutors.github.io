58de7da7c9d394ed2d2a306f62cc75c4ae191c57
Aarush Parikh <parikhaarush@gmail.com>
2023-06-15 16:51:00 -0700

    attempt 3.6

--- Backend/frontlogin.js
+++ Backend/frontlogin.js
@@ -1,6 +1,6 @@
 
 // const loginService = require('./service/login'); // connection to 
-const Amplify = require('aws-amplify');
+// const Amplify = require('aws-amplify');
 Amplify.configure({
     Auth:{
         cognito: {
