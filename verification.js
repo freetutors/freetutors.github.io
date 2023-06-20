e647902f1de3a1c87262eff7d928a04f8ccc7de9
Aarush Parikh <parikhaarush@gmail.com>
2023-06-20 16:25:20 -0700

    attempt 1 Verification

--- verification.js
+++ verification.js
@@ -17,3 +17,30 @@ AWS.config.update({
 
 var cognito = new AWS.CognitoIdentityServiceProvider(); //connection to cognito identiy
 AWS_SDK_LOAD_CONFIG=1
+
+function verifyUser(email, verificationCode) {
+    const params = {
+      ClientId: clientId,
+      Username: email,
+      ConfirmationCode: verificationCode,
+    };
+  
+    cognito.confirmSignUp(params, function (err, data) {
+      if (err) {
+        console.log(err, err.stack);
+        alert('Verification failed. Please try again.');
+      } else {
+        console.log(data);
+        alert('Verification successful! You can now sign in.');
+        // Redirect the user to the sign-in page or perform any desired action
+      }
+    });
+  }
+  
+  document.querySelector('.verification-send').addEventListener('click', function () {
+    console.log("clicked")
+    const verificationCode = this.value;
+    const email = 'YOUR_USER_EMAIL'; // Replace with the user's email or retrieve it from your application's state
+  
+    verifyUser(email, verificationCode);
+  });
