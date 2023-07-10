406685dcc394fdb3054dad08efe65dd746c7225d
Aarush Parikh <parikhaarush@gmail.com>
2023-07-10 15:41:12 -0700

    changes

--- API/service/answer.js
+++ API/service/answer.js
@@ -6,7 +6,7 @@ AWS.config.update({
 })
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 const tableName = 'Freetutor-Question' // connection to database and user table
-var answerId = uuidv4()
+const answerId = uuidv4()
 
 const util = require('../utils/util'); // every period shows how much higher you have to go on file levels. this has to periods because u have to go outside of the folder, one means that is in the same folder
 
@@ -17,10 +17,13 @@ async function createAnswer(answerData) {
     };
     
     const questionData = await dynamodb.get(getParams).promise();
-    
+    console.log(questionData)
+    if (!questionData.Item.answersInfo) {
+        questionData.Item.answersInfo = [];
+      }
     // Update the answers array with the new answer
     const newAnswer = { answerId: answerId, body: answerData.body, author: answerData.author, rating: 0, timestamp: new Date().toISOString()};
-    questionData.Item.answers.push(newAnswer);
+    questionData.Item.answersInfo.push(newAnswer);
     
     // Save the updated question item back to DynamoDB
     const updateParams = {
@@ -29,5 +32,11 @@ async function createAnswer(answerData) {
     };
     
     await dynamodb.put(updateParams).promise();
+    const response = {
+        question: questionData,
+        answer: newAnswer
+    }
+    return util.buildResponse(200, response);
+
 }
 module.exports.createAnswer = createAnswer     
--- API/service/getAnswer.js
+++ API/service/getAnswer.js
@@ -4,6 +4,5 @@ AWS.config.update({
 })
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 const tableName = 'Freetutor-Question' // connection to database and user table
-const questionId = uuidv4()
 
 const util = require('../utils/util'); // every period shows how much higher you have to go on file levels. this has to periods because u have to go outside of the folder, one means that is in the same folder
