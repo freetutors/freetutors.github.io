cc4532e985938b045bdbefc0e47eb873549d5999
rokkc <yashpattekar@gmail.com>
2023-07-15 16:16:05 -0700

    added inbox

--- loginsignupbuttons.js
+++ loginsignupbuttons.js
@@ -1,43 +1,54 @@
-//This code will be used for changing the login and signup button area
-//to the profile area
-const username = localStorage.getItem("CognitoIdentityServiceProvider.lact4vt8ge7lfjvjetu1d3sl7.LastAuthUser")
-// const username = "dfghjk" //for testing
-console.log(username)
-if (username != null){
-    // const username = localStorage.getItem("CognitoIdentityServiceProvider.lact4vt8ge7lfjvjetu1d3sl7.LastAuthUser")
-    document.getElementById("loginSignupArea").innerHTML =
-    `
-    <div class = "profileButton">
-        <img class = "inboxHome" src="inbox.png">
-        <div class="userInfoContainerHome">
-          <img class = "profilePicHome" src="profileDefault.jpg">
-          <p class = "usernameOnProfileButton">${username}</p>
-        </div
+const username = localStorage.getItem("CognitoIdentityServiceProvider.lact4vt8ge7lfjvjetu1d3sl7.LastAuthUser");
 
+
+function inboxDisplay() {
+
+  const inbox = document.querySelector('.inbox');
+
+  if (inbox.style.display === "none") {
+    inbox.style.display = "block";
+  } else {
+    inbox.style.display = "none";
+  }
+}
+
+if (username != null) {
+  const profileButton = document.createElement('div');
+  profileButton.classList.add('profileButton');
+  profileButton.innerHTML = `
+    <img class="inboxButton" src="inbox.png" onclick="inboxDisplay()">
+    <div class="userInfoContainerHome" onclick="location.href='https://freetutors.github.io/profile'">
+      <img class="profilePicHome" src="profileDefault.jpg">
+      <p class="usernameOnProfileButton">${username}</p>
     </div>
-    `
-    document.querySelector(".profilePicHome").
-    addEventListener("click", () => {
-        window.location='https://freetutors.github.io/profile'
-    });
-    document.querySelector(".usernameOnProfileButton").
-    addEventListener("click", () => {
-        window.location='https://freetutors.github.io/profile'
-    });   
-}
-else{
-    document.getElementById("loginSignupArea").innerHTML =
-    `<button class="button login-button">Log in</button>
-    <button class="button signup-button">Sign up</button>`
-    //setting login button to redirect
-    document.querySelector(".login-button").
-    addEventListener("click", () => {
-        window.location='https://freetutors.github.io/login'
-    }); 
-    //setting signup button to redirect
-    document.querySelector(".signup-button").
-    addEventListener("click", () => {
-        window.location='https://freetutors.github.io/signup'
-    });
-}
+  `;
 
+
+  const inbox = document.createElement('div');
+  inbox.classList.add('inbox');
+  inbox.innerHTML = `
+    <div class="inboxLetters">aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</div>
+    <div class="inboxTime">Today</div>
+    <hr class="inboxLine">
+    <div class="inboxLetters">aaa</div>
+    <div class="inboxTime">December 22</div>
+  `;
+
+  profileButton.appendChild(inbox);
+
+  document.getElementById("loginSignupArea").innerHTML = '';
+  document.getElementById("loginSignupArea").appendChild(profileButton);
+
+} else {
+  document.getElementById("loginSignupArea").innerHTML =
+  `<button class="button login-button">Log in</button>
+   <button class="button signup-button">Sign up</button>`;
+
+  document.querySelector(".login-button").addEventListener("click", () => {
+    window.location.href = 'https://freetutors.github.io/login';
+  });
+
+  document.querySelector(".signup-button").addEventListener("click", () => {
+    window.location.href = 'https://freetutors.github.io/signup';
+  });
+}
