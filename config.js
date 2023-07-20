acba05fe18595ebccf738b91635a7a971a16e56b
Aarush Parikh <parikhaarush@gmail.com>
2023-07-19 18:08:17 -0700

    es not working

---
+++ config.js
@@ -0,0 +1,17 @@
+var config  = {
+    apiUrlcreateConfig : "https://k4zqq0cm8d.execute-api.us-west-1.amazonaws.com/beta/create",
+    apiUrlgetConfig  : "https://k4zqq0cm8d.execute-api.us-west-1.amazonaws.com/beta/getquestion",
+    healthConfig  : "https://k4zqq0cm8d.execute-api.us-west-1.amazonaws.com/beta/health",
+    apiUrlupdateConfig  : "https://k4zqq0cm8d.execute-api.us-west-1.amazonaws.com/beta/updatequestion",
+    apiUrlanswerConfig  : "https://k4zqq0cm8d.execute-api.us-west-1.amazonaws.com/beta/createanswer",
+    apiUrlanswerUpdateConfig  : "https://k4zqq0cm8d.execute-api.us-west-1.amazonaws.com/beta/updateanswer",
+    apiUrlgetUserConfig  : "https://d487bezzog.execute-api.us-west-1.amazonaws.com/beta/get",
+    // Import the necessary AWS SDK components
+    poolIdConfig  :'us-west-1_w3se6DxlL', //getting info from cognito
+    clientIdConfig  :'lact4vt8ge7lfjvjetu1d3sl7',
+    regionConfig  : 'us-west-1',
+    accessKeyConfig  : "AKIAS6EY4GUSOJWYQPUN",
+    secretKeyConfig  : "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",
+    
+    apiUrlCreateUserConfig: "https://d487bezzog.execute-api.us-west-1.amazonaws.com/beta/create"
+}
