5278ec402a5f949eae3b58c8d69c37c42fe8eeee
rokkc <yashpattekar@gmail.com>
2023-07-21 15:21:50 -0700

    working again aaaaaaaa pls help me

--- search.js
+++ search.js
@@ -1,43 +1,71 @@
-apiUrlget  = "https://k4zqq0cm8d.execute-api.us-west-1.amazonaws.com/beta/getquestion"
+async function getAllQuestions() {
+  const questions = [];
+  for (const subject of searchSubjects) {
+    const subjectQuestionList = await getQuestionListSubject(subject);
+    for (const question of subjectQuestionList) {
+      questions.push({id: question.questionId, title: question.title});
+    }
+  }
+  return questions;
+}
 
-// async function getAllQuestions() {
-//   allQuestionsList = [];
-//   for (const subject of subjects) {
-//     subjectQuestionList = await getQuestionListSubject(subject);
-//     for (const question of subjectQuestionList) {
-//       allQuestionsList.push(question.title)
-//     }
-//   }
+async function getQuestionListSubject(subject) {
+    const url = new URL(`${apiUrlget}?subject=${subject}`);
+    const questionList = await fetch(url,  {
+        mode: "cors",
+        method: "GET",
+        headers: {
+        "Content-Type": "application/json",
+        },
+    }).then(response => response.json());
+    return questionList.questionList
+    }
 
-//   return allQuestionsList
-// }
+const searchSubjects = [
+  //htmlSection
+  "chemistry",
+  "biology",
+  "physics",
+  "english",
+  "history",
+  "geography",
+  "foreign language",
+  "computer science",
+  "physical education",
+  "math",
+];
 
-// async function showQuestionColumn(subject){
-//     const questionList = await getQuestionListSubject(subject)
-// }
+(async () => {
+    const questions = await getAllQuestions();
+    console.log(questions)
 
-// async function getQuestionListSubject(subject) {
-//     const url = new URL(`${apiUrlget}?subject=${subject}`);
-//     const questionList = await fetch(url,  {
-//         mode: "cors",
-//         method: "GET",
-//         headers: {
-//         "Content-Type": "application/json",
-//         },
-//     }).then(response => response.json());
-//     return questionList.questionList
-// }
+    const client = new MeiliSearch({
+        host: 'http://127.0.0.1:7700',
+        apiKey: 'kQE9SvunfTVx7NsGiqpq1NKRip-HKkGof55F0WOt77k',
+    });
 
-// const subjects = [ //htmlSection
-//     "chemistry",
-//     "biology",
-//     "physics",
-//     "english",
-//     "history",
-//     "geography",
-//     "foreign language",
-//     "computer science",
-//     "physical education",
-//     "math",
-//   ];
+    const index = client.index('questions')
+
+    async function performSearch(inputValue) {
+      const search = await index.search(inputValue);
+      console.log(search.hits);
+    }
+
+    if (!index) {
+        try {
+            const newIndex = await createIndex(client, indexName);
+            console.log('Index created:', newIndex);
+            index = client.getIndex(indexName);
+        } catch (error) {
+            console.error('Error creating index:', error);
+        }
+    }
+
+    // Search bar input event listener
+    const searchBar = document.querySelector('.search-bar');
+    searchBar.addEventListener('input', (event) => {
+        performSearch(event.target.value);
+    });
+
+})();
 
