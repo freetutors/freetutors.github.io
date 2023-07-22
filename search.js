32127db06c4466ecae6f90f94c7e5651e27b65de
rokkc <yashpattekar@gmail.com>
2023-07-22 06:13:41 -0700

    PLEASE 🙏🙏🙏

--- search.js
+++ search.js
@@ -46,15 +46,20 @@ const searchSubjects = [
 ];
 
 (async () => {
-    /*searchResultContainer.innerHTML = ''*/
-    const questions = await getAllQuestions();
+  /*searchResultContainer.innerHTML = ''*/
+  const questions = await getAllQuestions();
+  console.log(questions)
 
-    const client = new MeiliSearch({
-        host: 'http://127.0.0.1:7700',
-        apiKey: 'kQE9SvunfTVx7NsGiqpq1NKRip-HKkGof55F0WOt77k',
-    });
+  const client = new MeiliSearch({
+      host: 'http://52.53.226.42',
+      apiKey: 'ZWE3ZGM2YmFmN2JkMjU0ZTBhZWViY2Jm',
+  });
 
-    const index = client.index('questions')
+
+  const index = client.index('questionIndex')
+
+  let response = await index.addDocuments(questions)
+  console.log(response)
 
     async function performLiveSearch(inputValue) {
       const search = await index.search(inputValue);
@@ -71,15 +76,6 @@ const searchSubjects = [
       }
     }
 
-    if (!index) {
-        try {
-            const newIndex = await createIndex(client, indexName);
-            console.log('Index created:', newIndex);
-            index = client.getIndex(indexName);
-        } catch (error) {
-            console.error('Error creating index:', error);
-        }
-    }
 
     // Search bar input event listener
     const searchBar = document.querySelector('.search-bar');
@@ -99,6 +95,3 @@ searchBar.addEventListener('blur', () => {
   // Set the display property of the search result container back to its original value
   searchResultContainer.style.display = 'none'; // Or 'initial', 'flex', etc., depending on its original display value
 });
-
-
-
