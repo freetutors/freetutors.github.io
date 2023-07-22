e9ec89f8a9dc64dbcfc168966e3e6cf1e4c494a4
Aarush Parikh <parikhaarush@gmail.com>
2023-07-22 12:30:05 -0700

    amplify is so stupid

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ let data
 // import {from} from 'rxjs'
 async function getOptimizeConfig() {
     try {
-        const response = await fetch('./.secret/optimize.json');
+        const response = await fetch('./freetutors.github.io/.secret/optimize.json');
         console.log(response)
         const json = await response.json();
         console.log(json)
