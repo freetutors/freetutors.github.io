a975b5f040d5ad7fc2c9a2114b9ed96f66394ee2
rokkc <yashpattekar@gmail.com>
2023-07-24 00:32:36 -0700

    random commits

--- search.js
+++ search.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 searchResultContainer = document.querySelector(".possibleSearchResultContainer")
 possibleSearchResult = document.querySelector(".possibleSearchResult")
 
@@ -6,6 +7,13 @@ apiUrlget  = "https://k4zqq0cm8d.execute-api.us-west-1.amazonaws.com/beta/getque
 async function getAllQuestions() {
   const questions = [];
   for (const subject of searchSubjects) {
+=======
+const apiUrlget = "https://k4zqq0cm8d.execute-api.us-west-1.amazonaws.com/beta/getquestion";
+
+async function getAllQuestions() {
+  const questions = [];
+  for (const subject of subjects) {
+>>>>>>> Stashed changes
     const subjectQuestionList = await getQuestionListSubject(subject);
     for (const question of subjectQuestionList) {
       questions.push({id: question.questionId, title: question.title});
@@ -15,6 +23,7 @@ async function getAllQuestions() {
 }
 
 async function getQuestionListSubject(subject) {
+<<<<<<< Updated upstream
     const url = new URL(`${apiUrlget}?subject=${subject}`);
     const questionList = await fetch(url,  {
         mode: "cors",
@@ -105,3 +114,62 @@ searchBar.addEventListener('focus', () => {
 searchBar.addEventListener('blur', () => {
 
 });*/
+=======
+  const url = new URL(`${apiUrlget}?subject=${subject}`);
+  const questionList = await fetch(url, {
+    mode: "cors",
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }).then((response) => response.json());
+  return questionList.questionList;
+}
+
+const subjects = [
+  //htmlSection
+  "chemistry",
+  "biology",
+  "physics",
+  "english",
+  "history",
+  "geography",
+  "foreign language",
+  "computer science",
+  "physical education",
+  "math",
+];
+
+(async () => {
+    const questions = await getAllQuestions();
+
+    const client = new MeiliSearch({
+        host: 'http://127.0.0.1:7700',
+        apiKey: '8Gqjb5Plux9O5lB9UAjqdE_9WjCUNELR4K2WnPbOAhA',
+    });
+
+    let index = await client.getIndex('questions');
+
+    async function performSearch(inputValue) {
+      const search = await index.search(inputValue);
+      console.log(search.hits);
+    }
+
+    if (!index) {
+        try {
+            const newIndex = await createIndex(client, indexName);
+            console.log('Index created:', newIndex);
+            index = client.getIndex(indexName);
+        } catch (error) {
+            console.error('Error creating index:', error);
+        }
+    }
+
+    // Search bar input event listener
+    const searchBar = document.querySelector('.search-bar');
+    searchBar.addEventListener('input', (event) => {
+        performSearch(event.target.value);
+    });
+
+})();
+>>>>>>> Stashed changes
