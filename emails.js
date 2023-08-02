9700a062ba1c8d4f7fd6139c5939593877558bd8
Aarush Parikh <parikhaarush@gmail.com>
2023-08-02 09:02:41 -0700

    email stuffs

--- emails.js
+++ emails.js
@@ -2,21 +2,23 @@ import config from "./config.js";
 //sending email to lamda and then to owners
 var path = window.location.pathname;
 var pageName = path.split("/").pop();
-
 const questionBox = document.querySelector(".question_box")
 const commentsBox = document.querySelector(".comments_box")
 var dropbtn = document.querySelector(".dropbtn");
 
-AWS.config.update({
+const poolId =config.poolId //getting info from cognito
+const region = config.region
+AWS.config.region = region; //telling what region to search
+AWS.config.credentials = new AWS.CognitoIdentityCredentials({ //COnnecting to pool
+  IdentityPoolId: poolId 
+});
+
+AWS.config.update({ //initializing aws services
   region: 'us-west-1',
   credentials: new AWS.Credentials(config.accessKey, config.secretKey),
 });
+var cognito = new AWS.CognitoIdentityServiceProvider(); //connection to cognito identiy
 
-const sendEmailButton = document.querySelector(".buttons");
-
-const functionName = 'sendEmail';
-
-const lambda = new AWS.Lambda();
 const username = localStorage.getItem("CognitoIdentityServiceProvider.lact4vt8ge7lfjvjetu1d3sl7.LastAuthUser")
 if (username == null){
   if(window.confirm("Please Log In to Contact Us"));{
@@ -39,6 +41,12 @@ async function getUserCognito(username) { //getting cognito info
 const user = await getUserCognito(username) //getting user info with previous username
 const email = "          Account Email:" + user.UserAttributes[4].Value //letting us know ur email
 
+const sendEmailButton = document.querySelector(".buttons");
+
+const functionName = 'sendEmail';
+
+const lambda = new AWS.Lambda();
+
 sendEmailButton.addEventListener("click", () => {
 
   if (pageName == 'suggestions.html') {
@@ -51,7 +59,7 @@ sendEmailButton.addEventListener("click", () => {
 
   var subject = questionBox.value.trim().replace(/\r?\n|\r/g, '').replace(/"/g, "‟").replace(/"/g, '⁄').replace(/\\/g, '＼');
 
-  var body = commentsBox.value.trim().replace(/\r?\n|\r/g, ' ').replace(/"/g, "‟").replace(/"/g, ' ⁄').replace(/\\/g, '＼') + email;
+  var body = commentsBox.value.trim().replace(/\r?\n|\r/g, ' ').replace(/"/g, "‟").replace(/"/g, ' ⁄').replace(/\\/g, '＼');
 
   const payload = {
     "body": "{\"subject\": \"" + feedbackType + subject + "\", \"body\": \"" + body + "\"}"
@@ -69,6 +77,6 @@ sendEmailButton.addEventListener("click", () => {
       console.log('Response from Lambda:', data.Payload);
     }
   });
-  alert("Suggestion Sent")
+  alert("Message Sent")
   window.location = '/'
 });
