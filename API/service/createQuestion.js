34c83f18db4f9a41c13b2cb0146e5a982804b20c
Aarush Parikh <parikhaarush@gmail.com>
2023-09-05 21:29:19 -0700

    fix for question bug

--- API/service/createQuestion.js
+++ API/service/createQuestion.js
@@ -6,7 +6,7 @@ AWS.config.update({
 })
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 const tableName = 'Freetutor-Question' // connection to database and user table
-const questionId = randomizeUUIDv4()
+var questionId = randomizeUUIDv4()
 function randomizeUUIDv4() {
     // Generate a new UUIDv4
     const originalUUID = uuidv4();
@@ -28,6 +28,25 @@ function randomizeUUIDv4() {
   
     return randomizedUUID;
   }
+
+  async function getQuestionById(questionId) {
+    const params = {
+      TableName: questionTable,
+      KeyConditionExpression: "#Id = :questionId",
+      ExpressionAttributeNames: {
+        "#Id": "questionId"
+      },
+      ExpressionAttributeValues: {
+        ":questionId": questionId
+      },
+      Limit: 10
+    };
+  
+      // Query DynamoDB for the questions.
+      const result = await dynamodb.query(params).promise();
+  // Return the questions.
+      return result.Items;
+  }
 const util = require('../utils/util'); // every period shows how much higher you have to go on file levels. this has to periods because u have to go outside of the folder, one means that is in the same folder
 
 async function createQuestion(questionData) {
@@ -37,29 +56,42 @@ async function createQuestion(questionData) {
     const subject = questionData.subject
     const pfp = questionData.pfp
     const timestamp = new Date().toISOString();
-    if (!title || !body || !author || !timestamp) { //checking for all categories filled, || = or
-    return util.buildResponse(401, { // any arbitrary error number works BUT NOT 200 or 404
-        message: 'Missing Info'
-    })
-    }
-    const question = { //question details
-        questionId: questionId, //so we can pull it later
-        title: title,
-        body: body,
-        author: author.trim(), //username trimmed to get rid of whtie space
-        rating: 0,
-        views: 0,
-        answers: 0,
-        pfp: pfp,
-        timestamp: timestamp,
-        subject: subject
-    }
-    await saveQuestion(question)
-    const response = {
-        question: question,
-        questionId: questionId
-    }
-    return util.buildResponse(200, response);
+    do {
+        questionId = randomizeUUIDv4();
+        const duplicateCheck = await getQuestionById(questionId);
+        if (duplicateCheck.length === 0) {
+            break; // Exit the loop if the generated questionId is unique
+        }else{
+            setTimeout(function(){
+                console.log("need to wait")
+            },1000)
+        }
+        } while (true);
+    
+        if (!title || !body || !author || !timestamp) { //checking for all categories filled, || = or
+            return util.buildResponse(401, { // any arbitrary error number works BUT NOT 200 or 404
+                message: 'Missing Info'
+            })
+            }
+            const question = { //question details
+                questionId: questionId, //so we can pull it later
+                title: title,
+                body: body,
+                author: author.trim(), //username trimmed to get rid of whtie space
+                rating: 0,
+                views: 0,
+                answers: 0,
+                pfp: pfp,
+                timestamp: timestamp,
+                subject: subject
+            }
+            await saveQuestion(question)
+            const response = {
+                question: question,
+                questionId: questionId
+            }
+            return util.buildResponse(200, response);
+    
 }
 async function saveQuestion(question){ //saving new users to database
     const params = {
