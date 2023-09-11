e915e41355dec7d285c682015a9ba9afcfdf1940
rokkc <yashpattekar@gmail.com>
2023-09-11 16:31:56 -0700

    fixed search

--- searchResults.js
+++ searchResults.js
@@ -4,30 +4,38 @@ const questionsList = document.querySelector(".questions_list")
 const apiUrlgetUser = config.apiUrlgetUser;
 
 AWS.config.update({
-  region: config.region,
-  accessKeyId: config.accessKet,
-  secretAccessKey: config.secretKey,
+    region: config.region,
+    accessKeyId: config.accessKet,
+    secretAccessKey: config.secretKey,
 });
 
 document.addEventListener('click', function(e) {
-  var clickedElement = (e.target)
-  if (clickedElement.className == "box text_box") {
-    const id = clickedElement.getAttribute('alt')
-    window.location = `viewQuestion?questionId=${id}`;
-  }
+    var clickedElement = (e.target)
+    if (clickedElement.id == "text_box_question_content") {
+        var alt = clickedElement.getAttribute('alt')
+        window.location = `viewQuestion?questionId=${alt}`;
+    }
+    else if (clickedElement.id == "text_box_pfp") {
+        var alt = clickedElement.getAttribute('alt')
+        window.location = `profile?username=${alt}`;
+    }
+    else if (clickedElement.id == "asked_by_line") {
+        var alt = clickedElement.getAttribute('alt')
+        window.location = `profile?username=${alt}`;
+    }
 }, false);
 
 const urlParams = new URLSearchParams(window.location.search)
 async function getUser(username){ //getting user info from dynamo
-  const url = new URL(`${apiUrlgetUser}?username=${username}`);
-  const user = await fetch(url,  {
-      mode: "cors",
-      method: "GET",
-      headers: {
-      "Content-Type": "application/json",
-      },
-  }).then(response => response.json());
-  return user
+    const url = new URL(`${apiUrlgetUser}?username=${username}`);
+    const user = await fetch(url,  {
+        mode: "cors",
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+        },
+    }).then(response => response.json());
+    return user
 }
 function getTimeDifference(timestamp) {
     const currentTime = new Date();
@@ -52,7 +60,7 @@ function getTimeDifference(timestamp) {
     } else {
         return `${seconds} second${seconds !== 1 ? 's' : ''} ago`;
     }
-    }
+}
 
 var query = urlParams.get('query');
 
@@ -62,113 +70,78 @@ var query = urlParams.get('query');
         apiKey: config.searchKey,
     });
 
-  const index = client.index('questionListIndex3')
-  const search = await index.search(query);
+    const index = client.index('questionListIndex3')
+    const search = await index.search(query);
 
-  let idList = []
-  for (const id of search.hits) {
-    idList.push(id.id)
-  }
+    let idList = []
+    for (const id of search.hits) {
+        idList.push(id.id)
+    }
 
-  const dynamodb = new AWS.DynamoDB.DocumentClient();
+    const dynamodb = new AWS.DynamoDB.DocumentClient();
 
-  var table = "Freetutor-Question";
+    var table = "Freetutor-Question";
 
-  const tableName = 'Freetutor-Question';
-  const itemIds = idList;
-  const params = {
-    RequestItems: {
-      [tableName]: {
-        Keys: itemIds.map(questionId => ({ questionId })),
-      },
-    },
-  };
+    const tableName = 'Freetutor-Question';
+    const itemIds = idList;
+    const params = {
+        RequestItems: {
+            [tableName]: {
+                Keys: itemIds.map(questionId => ({ questionId })),
+            },
+        },
+    };
 
-  dynamodb.batchGet(params, function(err, data) {
-      if (err) {
-          console.error("Unable to read item. Error JSON:", JSON.stringify(err,
-                  null, 2));
-      } else {
-          const items = data.Responses[tableName].map(item => item);
+    dynamodb.batchGet(params, function(err, data) {
+        if (err) {
+            console.error("Unable to read item. Error JSON:", JSON.stringify(err,
+                null, 2));
+        } else {
+            const items = data.Responses[tableName].map(item => item);
 
-          items.sort((a, b) => {
-            const indexA = idList.indexOf(a.questionId);
-            const indexB = idList.indexOf(b.questionId);
+            items.sort((a, b) => {
+                const indexA = idList.indexOf(a.questionId);
+                const indexB = idList.indexOf(b.questionId);
 
-            return indexA - indexB;
-          });
+                return indexA - indexB;
+            });
 
-          placeQuestionBoxes(items)
-          
-      }
-  });
-async function placeQuestionBoxes(items) {
-  for (const question of items) {
-    const user = await getUser(question.author)
-    const pfp = user.user[0].pfp
-    var displayedImage = ""
-        if (pfp == null){ //getting pfp, if pfp is none it will user default
-          displayedImage = "placeholder_pfp.png"
+            placeQuestionBoxes(items)
         }
-        else{
-          displayedImage = `data:image/png;base64,${pfp}`
+    });
+
+    async function placeQuestionBoxes(items) {
+        for (const question of items) {
+            const user = await getUser(question.author)
+            const pfp = user.user[0].pfp
+            var displayedImage = ""
+            if (pfp == null){ //getting pfp, if pfp is none it will user default
+                displayedImage = "placeholder_pfp.png"
+            }
+            else{
+                displayedImage = `data:image/png;base64,${pfp}`
+            }
+            if (!question.answersInfo){
+                var answers = 0
+
+            }else{
+                var answers = question.answersInfo.length
+            }
+            questionsList.innerHTML +=
+                `<div class="box text_box">
+             <img id="text_box_pfp" alt="${question.author}" src="${displayedImage}">
+             <div id="text_box_question_content" alt="${question.questionId}">${question.title}</div>
+             <div id="asked_by_line" alt="${question.author}">asked by ${question.author}, ${getTimeDifference(question.timestamp)}</div>
+             <div id="answered_by_line">Be the first to answer!</div>
+             <div class="question_stats">
+             <div id="question_stats_items">${answers} Answers</div>
+             <div id="question_stats_items">${question.views} views</div>
+             <div id="question_stats_items">${question.rating} rating</div>
+          </div>`
         }
-        if (!question.answersInfo){
-          var answers = 0
-
-      }else{
-          var answers = question.answersInfo.length
-      }
-      var author = question.author
-      var unformattedAuthor = question.author      
-      author = author.replace(/\./g,"")
-      if (answers != 0){
-        questionsList.innerHTML += //sending html info
-        `<div class="box text_box">
-        <!-- pfp -->
-        <img id="global_pfp" class = "pfp${author}" src="${displayedImage}" alt="user_pfp" onclick="window.location='/profile?username=${unformattedAuthor}'">
-        <div id="text_box_question_content" onclick="window.location = '/viewQuestion?questionId=${question.questionId}'">${question.title}</div>
-        <div id="asked_by_line">asked by <a href="https://www.freetutors.net/profile?username=${unformattedAuthor}">${author}, ${getTimeDifference(question.timestamp)}</a></div>
-        <div id="answered_by_line">Add to the conversation!</div>
-        <div class="question_stats">
-          <div id="question_stats_items">${answers} Answers</div>
-          <div id="question_stats_items">${question.views} views</div>
-          <div id="question_stats_items">${question.rating} rating</div>
-        </div>`
-          }
-          else{
-              questionsList.innerHTML += //sending html info
-              `<div class="box text_box">
-      <!-- pfp -->
-      <img id="global_pfp" class = "pfp${author}" src="${displayedImage}" alt="user_pfp" onclick="window.location='/profile?username=${unformattedAuthor}'">
-      <div id="text_box_question_content" onclick="window.location = '/viewQuestion?questionId=${question.questionId}&title=${title}'">${question.title}</div>
-      <div id="asked_by_line">asked by <a href="https://www.freetutors.net/profile?username=${unformattedAuthor}">${author}, ${getTimeDifference(question.timestamp)}</a></div>
-      <div id="answered_by_line">Be the first to answer!</div>
-      <div class="question_stats">
-        <div id="question_stats_items">${answers} Answers</div>
-        <div id="question_stats_items">${question.views} views</div>
-        <div id="question_stats_items">${question.rating} rating</div>
-      </div>`
-      }
-
-        // questionsList.innerHTML +=
-        //   `<div class="box text_box" alt="${question.questionId}">
-        //      <img id="text_box_pfp" src="${displayedImage}">
-        //      <div id="text_box_question_content">${question.title}</div>
-        //      <div id="asked_by_line">asked by ${question.author}, ${getTimeDifference(question.timestamp)}</div>
-        //      <div id="answered_by_line">Be the first to answer!</div>
-        //      <div class="question_stats">
-        //      <div id="question_stats_items">${answers} Answers</div>
-        //      <div id="question_stats_items">${question.views} views</div>
-        //      <div id="question_stats_items">${question.rating} rating</div>
-        //   </div>`
-          console.log(question)
-          console.log(question.answerInfo)
-  }
-}
+    }
 
 
 })();
-
 
 
