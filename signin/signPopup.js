18c6b1b3f5db6f4488ee02879b51551fb8844542
Aarush Parikh <parikhaarush@gmail.com>
2023-09-19 10:18:42 -0700

    almost done dont touch roshan please por favor

--- signin/signPopup.js
+++ signin/signPopup.js
@@ -1,5 +1,4 @@
 const signPopUp = document.querySelector('.signPopUp');
-const welcomePopUp = document.querySelector('.welcomePopUp')
 const askQuestionButton = document.querySelector('.ask-question-button')
 var path = window.location.pathname;
 var pageName = path.split("/").pop();
@@ -32,33 +31,34 @@ function getCookie(name) {
 }
 
 if (!checkCookieExists("usage") || getCookie("usage") == "new"){
-   setCookie('usage', 'first', 365) //un comment this when the popup is ready this is for testing
+  //  setCookie('usage', 'first', 365) //un comment this when the popup is ready this is for testing
    console.log('hi')
-   welcomePopUp.innerHTML = `
+   signPopUp.innerHTML = `
        <p class="welcomeText welcome"><b>Welcome to FreeTutors.net!</b></p>
-       <img class ="welcomeMascot left" id="mascot" src="final_mascot.svg" alt="mascot" style="left: 10px; padding-up: 20px; display: inline-block; width: 120px;">
-       <p class="signPopUpText" style="position: relative; margin-top: 0px; padding-left: 20px; display: inline-block; width: 300px; font-size:24px; text-align: center; align: center; overflow-wrap:break-word; word-wrap:break-word; vertical-align: top;">We're a <b>FREE</b> to use Q&A site for all your school needs.
+       <img class ="welcomeMascot left" id="mascot" src="final_mascot.svg" alt="mascot" style="position: absolute; left: 10px; up: 10px;">
+       <p class="welcomeText right" style="padding-left: 200px; right:10px; up:50px; left:100px; font-size:24px; text-align:center; overflow-wrap:break-word; word-wrap:break-word;">We're a <b>FREE</b> to use Q&A site for all your school needs.
        <br/>
        <b>NO SUBSCRIPTIONS.</b>
        <br/> 
        It's simple. Just ask any question and get answers
        </p>
-       <br/>
        <p class="welcomeText end" style="font-size:24px;">Need volunteer hours? <b>Coming soon on FreeTutors!</b></p>
-       <button class="welcomeSignUpButton" style="height: 80px; width: 200px; background-color: var(--secondary-color); border-radius: 12.5px;" onclick="window.location='signUp'">Sign Up</button>
-       <!-- 
-       Buttons are not styled properly/fully
-       -->
-       <button class="welcomeAboutUsButton" style="height: 30px; width: 200px; background-color: var(--primary-color); border-radius: 12.5px; padding-bottom: 5px;" onclick="window.location='contactUs'">Contact Us</button>
-       <button class="welcomeAboutUsButton" style="height: 30px; width: 200px; background-color: var(--primary-color); border-radius: 12.5px; padding-top: 5px;" onclick="window.location='ourTeam'">About Us</button>
-       <div class="signPopUpX" onclick="document.querySelector('.welcomePopUp').style.display='none'"></div>
-     `;
-     welcomePopUp.style.display = 'block';
+       <div class="buttons">
+       <div class="button sign-up-button" onclick="window.location='/signup'">Sign Up</div>
+       <div class="button-column">
+         <div class="button contact-button"onclick="window.location='/contactUs'">Contact Us</div>
+         <div class="button about-button"onclick="window.location='/ourTeam'">About Us</div>
+       </div>
+     </div>
+     <p class="popup-link">Follow us <a class="insta" href = "https://www.instagram.com/freetutorsdotnet/">@freetutorsdotnet </a>for more updates</p>
+     <div class="signPopUpX" onclick="document.querySelector('.signPopUp').style.display='none'"></div>
+       `;
+     signPopUp.style.display = 'block';
  }
 if (username == null) {
   askQuestionButton.addEventListener('click', () => {
     signPopUp.innerHTML = `
-      <p class="signPopUpText" style="inset: 0">You must be logged in to ask a question.</p>
+      <p class="signPopUpText">You must be logged in to ask a question.</p>
       <div class="signPopUpLogin" onclick='location.href="login"'>Log in</div>
       <div class="signPopUpSignup" onclick='location.href="signup"'>Sign up</div>
       <div class="signPopUpX" onclick="document.querySelector('.signPopUp').style.display='none'"></div>
@@ -72,7 +72,7 @@ if (pageName = 'index.html') {
       }
   document.querySelector("#sign_up_as_tutor_button").addEventListener('click', () => {
     signPopUp.innerHTML = `
-      <p class="signPopUpText" style="inset: 0">You must be logged in to sign up as a tutor.</p>
+      <p class="signPopUpText">You must be logged in to sign up as a tutor.</p>
       <div class="signPopUpLogin" onclick='location.href="login"'>Log in</div>
       <div class="signPopUpSignup" onclick='location.href="signup"'>Sign up</div>
       <div class="signPopUpX" onclick="document.querySelector('.signPopUp').style.display='none'"></div>
@@ -82,7 +82,7 @@ if (pageName = 'index.html') {
 }
 if (pageName == 'createQuestion'){
   signPopUp.innerHTML = `
-    <p class="signPopUpText" style="inset: 0">You must be logged in to ask a question.</p>
+    <p class="signPopUpText">You must be logged in to ask a question.</p>
     <div class="signPopUpLogin" onclick='location.href="login"'>Log in</div>
     <div class="signPopUpSignup" onclick='location.href="signup"'>Sign up</div>
     <div class="signPopUpX" onclick="document.querySelector('signPopUp').style.display='none'"></div>
@@ -91,7 +91,7 @@ if (pageName == 'createQuestion'){
 }
 if (pageName == 'suggestions'){
   signPopUp.innerHTML = `
-    <p class="signPopUpText" style="inset: 0">You must be logged in to make a suggestion.</p>
+    <p class="signPopUpText">You must be logged in to make a suggestion.</p>
     <div class="signPopUpLogin" onclick='location.href="login"'>Log in</div>
     <div class="signPopUpSignup" onclick='location.href="signup"'>Sign up</div>
     <div class="signPopUpX" onclick="document.querySelector('signPopUp').style.display='none'"></div>
@@ -100,7 +100,7 @@ if (pageName == 'suggestions'){
 }
 if (pageName == 'contactUs'){
   signPopUp.innerHTML = `
-    <p class="signPopUpText" style="inset: 0">You must be logged in to contact us.</p>
+    <p class="signPopUpText">You must be logged in to contact us.</p>
     <div class="signPopUpLogin" onclick='location.href="login"'>Log in</div>
     <div class="signPopUpSignup" onclick='location.href="signup"'>Sign up</div>
     <div class="signPopUpX" onclick="document.querySelector('signPopUp').style.display='none'"></div>
