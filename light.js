2f06b622b8c10f0b4a42e6cb4265df2b214b6d60
Crypticccc <robotmaster8990@gmail.com>
2024-06-20 12:27:08 -0700

    Rework of light mode colors
    my eyes are saved

--- light.js
+++ light.js
@@ -21,34 +21,35 @@ return null;
 }
 
 const theme = getCookie("theme")
-var darkBlue = 'rgb(132, 175, 255)'
+var lightBlue = '#bfe9ffff'
 console.log(theme)
 if (theme=="light"){
     console.log(theme)
-    document.documentElement.style.setProperty('--primary-color', '#ffdaa2ff');
-    document.documentElement.style.setProperty('--background-color', '#fffaedff');
+    document.documentElement.style.setProperty('--primary-color', '#f5fbffff');
+    document.documentElement.style.setProperty('--background-color', '#ffffffff');
     document.documentElement.style.setProperty('--header-color', '#8dd8ffff');
-    document.documentElement.style.setProperty('--text-box-bar-color', darkBlue);
-    document.documentElement.style.setProperty('--secondary-color', '#8dd8ffff');
-    document.documentElement.style.setProperty('--tertiary-color', '#84CAEF');
-    document.documentElement.style.setProperty('--quatenary-color', '#4F92CA');
+    document.documentElement.style.setProperty('--text-box-bar-color', '#edf8ff');
+    document.documentElement.style.setProperty('--secondary-color', '#ebf7ff');
+    document.documentElement.style.setProperty('--tertiary-color', '#f4ffeb');
+    document.documentElement.style.setProperty('--quatenary-color', '#f4ffeb');
     document.documentElement.style.setProperty('--quinary-color', '#3875BE');
     document.documentElement.style.setProperty('--senary-color', '#295DB7');
-    document.documentElement.style.setProperty('--border-color', 'rgba(0,0,0,1)');
+    document.documentElement.style.setProperty('--border-color', '#d4faec');
     document.documentElement.style.setProperty('--text-color', '#3E3F4C');
-    document.documentElement.style.setProperty('--ask-question-button', darkBlue);
-    document.documentElement.style.setProperty('--search-bar', '#ffdaa2ff');
+    document.documentElement.style.setProperty('--ask-question-button', '#f4ffeb');
+    document.documentElement.style.setProperty('--search-bar', '#bfe9ffff');
     document.documentElement.style.setProperty('--subject-hover-color', 'rgb(154, 192, 255)')
     document.documentElement.style.setProperty('--menu-hover-color', 'rgb(141, 216, 255, 0.475)');
-    document.documentElement.style.setProperty('--profile-box-color', 'rgb(157, 191, 255)');
+    document.documentElement.style.setProperty('--profile-box-color', '#f4ffeb');
     document.documentElement.style.setProperty('--top-questions-box', '#ffdaa2ff');
     document.documentElement.style.setProperty('--qotw-hover-color', 'rgb(141, 216, 255, 0.275)');
     document.documentElement.style.setProperty('--top-questions-hover', 'rgb(141, 216, 255, 0.275)');
     document.documentElement.style.setProperty('--about-me-color', 'rgba(175, 175, 175, 0.349)');
-    document.documentElement.style.setProperty('--tool-bar', '#ffbf5f');
-    document.documentElement.style.setProperty('--sign-up-button', darkBlue);
-    document.documentElement.style.setProperty('--tutor-sign-up', darkBlue);
-    document.documentElement.style.setProperty('--underline-color', '#ffdaa2ff');
+    document.documentElement.style.setProperty('--tool-bar', '#f4ffeb');
+    document.documentElement.style.setProperty('--sign-up-button', '#f4ffeb');
+    document.documentElement.style.setProperty('--tutor-sign-up', '#8dd8ffff');
+    document.documentElement.style.setProperty('--underline-color', '#f7f7f7');
+    document.documentElement.style.setProperty('--divider-color', '#966afcff');
     ;
 }
 else{
