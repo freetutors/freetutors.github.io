f1063d5845b24bffe08b9ec46a833fad1b32522f
Aarush Parikh <parikhaarush@gmail.com>
2024-08-23 06:58:57 -0700

    test one

--- home.js
+++ home.js
@@ -1,6 +1,51 @@
 alert('8')
 // Import configuration from external file
-import config from "./config.js";
+// import config from "./config.js";
+let data
+async function getOptimizeConfig() {
+    try {
+        const response = await fetch('./optimize.json');
+        const json = await response.json();
+        data = processJSONData(json);
+
+    } catch (error) {
+      console.log(error);
+    }
+    return data;
+}
+function processJSONData(data) {
+    var data = {
+        apiUrlcreate : data.apiUrlcreate,
+        apiUrlget  : data.apiUrlget,
+        health  : data.health,
+        apiUrlupdate  : data.apiUrlupdate,
+        apiUrlanswer  : data.apiUrlanswer,
+        apiUrlanswerUpdate  : data.apiUrlanswerUpdate,
+        apiUrlgetUser  : data.apiUrlgetUser,
+        apiUrlupdateUserRating: data.apiUrlupdateUserRating,
+        apiUrlupdateAnswerRating: data.apiUrlupdateAnswerRating,
+        // Import the necessary AWS SDK components
+        poolId  : data.poolId, //getting info from cognito
+        clientId  :data.clientId,
+        region  : data.region,
+        accessKey  : data.accessKey,
+        secretKey  : data.secretKey,
+    
+        apiUrlCreateUser: data.apiUrlCreateUser,
+        apiUrlupdateUser: data.apiUrlupdateUser,
+        apiUrlupdateUserAnswer: data.apiUrlupdateUserAnswer,
+    
+        searchHost: data.searchHost,
+        searchKey: data.searchKey
+    }
+    return data
+}
+var range=[0]
+let old
+for (const i in range){
+    old = await getOptimizeConfig()
+}
+var config = old
 const qotwId = 'bcbb4292-68a9-90f3-a129-9a40e6994471' //put the id of the weekly question
 // Extract API URLs from configuration
 const apiUrlget = config.apiUrlget;
